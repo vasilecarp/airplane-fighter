84e01bebffaa82beabf798ef697a0493ec149b21
vasilecarp <vasilecarpconsulting@gmail.com>
2024-12-08 16:49:02 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import AirplaneFighter from './components/AirplaneFighter'
 
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100 py-8">
+    <div className="flex flex-col items-center justify-center bg-gray-100">
       <AirplaneFighter />
     </div>
-  )
+  );
 }
 
 export default App
